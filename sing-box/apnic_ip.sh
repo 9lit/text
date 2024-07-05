cc258923a2ca4f285e2e68a6bb760d72e39f7f0c
LongLit <xiongjum@outlook.com>
2024-07-05 14:32:14 +0800

    优化脚本文件

--- sing-box/apnic_ip.sh
+++ sing-box/apnic_ip.sh
@@ -2,9 +2,8 @@
 
 apnic_ip=$(curl http://ftp.apnic.net/stats/apnic/delegated-apnic-latest)
 cn_ipv4=$(echo "$apnic_ip" | awk -F '|' '/CN/&&/ipv4/ {print "\"" $4 "/" 32-log($5)/log(2) "\"" ","}')
-cn_ipv4=$(echo "${cn_ipv4::-1}")
 
-gfw='{"rules": [{"domain": ['${cn_ipv4}']}],"version": 1}'
+gfw='{"rules": [{"domain": ['${cn_ipv4::-1}']}],"version": 1}'
 
 if [ -z "$(which jq)" ]; then sudo apt update -y && sudo apt install aria2 -y; fi
 echo "$gfw" | jq . > ./sing-box/apnic_cn_ipv4.json
