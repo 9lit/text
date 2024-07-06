51aceec1e2409a5443cefb8a2f2e0bc19363fab4
LongLit <44801790+xiongJum@users.noreply.github.com>
2024-07-06 11:07:37 +0800

    将从重复的字符串修改为变量， 获取配置文件中的代理端口

--- aria2/config_aria2.sh
+++ aria2/config_aria2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# 设置全局变量
+
+## 基础路径配置
+URL="http://text.1210923.xyz/aria2"
+GITHUB="https://github.com/xiongJum/aria2.conf.git"
+ARIA2_CONFIG_PATH="/etc/aria2"
+NGINX_CONFIG_FILE="/etc/nginx/sites-enabled/aria2"
+
+## nginx 代理端口
+NGINX_PROXY_PORT=2053
+
 # 更新软件源
 sudo apt update -y
 
@@ -10,17 +21,22 @@ if [ -z "$(sudo which aria2c)" ]; then sudo apt install aria2 -y; fi
 # 创建配置文件
 ## 向 github 中获取 aria2 配置文件
 if [ -z "$(sudo which git)" ]; then sudo apt install git -y; fi
-sudo rm -rf /etc/aria2/ && sudo git clone https://github.com/xiongJum/aria2.conf.git /etc/aria2
+sudo rm -rf $ARIA2_CONFIG_PATH && sudo git clone "$GITHUB" $ARIA2_CONFIG_PATH
 ## 创建保存文件夹 和 会话文件
-sudo mkdir /etc/aria2/download/ && sudo touch /etc/aria2/aria2.session 
+sudo mkdir "${ARIA2_CONFIG_PATH}/download" && sudo touch "${ARIA2_CONFIG_PATH}/aria2.session" 
 ## 赋予可执行权限
-cd /etc/aria2 && sudo chmod +x delete.sh scrape.sh upload.sh
+cd $ARIA2_CONFIG_PATH && sudo chmod +x delete.sh scrape.sh upload.sh
 
 
 #使用 systemctl 管理 aria2 服务
-service="[Unit] \nDescription=Aria2 Service \nAfter=network.target \n\n[Service] \nExecStart=/usr/bin/aria2c --conf-path=/etc/aria2/aria2.conf \n\n[Install] \nWantedBy=default.target"
+service="[Unit] \nDescription=Aria2 Service \nAfter=network.target \n\n[Service] \nExecStart=/usr/bin/aria2c --conf-path=${ARIA2_CONFIG_PATH}/aria2.conf \n\n[Install] \nWantedBy=default.target"
 ## 将配置写入 aria2.service 并刷新 systemctl服务
 sudo bash -c "echo -e '$service' > /usr/lib/systemd/system/aria2.service" && sudo systemctl daemon-reload
+
+# 获取监听端口
+RPC_PORT=$(cat /etc/aria2/aria2.conf | grep -oP "rpc-listen-port=\d+" | grep -oP "\d+")
+BT_PORT=$(cat /etc/aria2/aria2.conf | grep -oP "listen-port=\d+" | grep -oP "\d+")
+DHT_PORT=$(cat /etc/aria2/aria2.conf | grep -oP "dht-listen-port=\d+" | grep -oP "\d+")
 
 
 # 使用 nginx 进行代理 aria rpc 服务
@@ -30,25 +46,28 @@ if [ "${flag,,}" = nginx ]; then
   ## 对 aria2 进行反代理
   if [ -z "$(sudo which nginx)" ]; then sudo apt install nginx -y; fi
   ## 设置 nginx 代理 配置
-  sudo cp /etc/aria2/nginx-aria2-config /etc/nginx/sites-available/aria2 && sudo ln /etc/nginx/sites-available/aria2 /etc/nginx/sites-enabled/aria2
+  ### 删除之前的配置 nginx 配置
+  sudo rm -rf $NGINX_CONFIG_FILE
+  ### 设置 nginx 代理 配置
+  sudo bash -c "curl ${URL}/nginx-config > ${NGINX_CONFIG_FILE}" && sudo ln ${NGINX_CONFIG_FILE} /etc/nginx/sites-enabled/aria2
   ## 关闭 原配置文件中的 rpc 远程访问权限
   sudo sed -i "s/rpc-allow-origin-all=true/rpc-allow-origin-all=false/g" /etc/aria2/aria2.conf && sudo sed -i "s/rpc-listen-all=true/rpc-listen-all=false/g" /etc/aria2/aria2.conf
   ## 重启 nginx 服务
   sudo systemctl restart nginx
 else
   sudo sed -i "s/rpc-allow-origin-all=false/rpc-allow-origin-all=true/g" /etc/aria2/aria2.conf && sudo sed -i "s/rpc-listen-all=false/rpc-listen-all=true/g" /etc/aria2/aria2.conf
-  echo "脚本退出, 不使用nginx 进行反代理" exit 0; fi
+fi
 
 # 开启 ufw 端口
 if [ -n "$(sudo which ufw)" ]; then 
   ## 放行 DHT 和 UDP tracker 监听端口
-  sudo ufw allow 51413/tcp && sudo ufw allow 51413/udp
+  sudo ufw allow ${BT_PORT}/tcp && sudo ufw allow ${DHT_PORT}/udp
   if [ "${flag,,}" = nginx ]; then 
     # 开放 nginx 代理端口
-    sudo ufw allow 2053/tcp && sudo ufw deny 6800/tcp
+    sudo ufw allow ${NGINX_PROXY_PORT}/tcp && sudo ufw deny ${RPC_PORT}/tcp
   else 
     # 开放 本地端口
-    sudo ufw allow 6800/tcp && sudo ufw deny 2053/tcp
+    sudo ufw allow ${RPC_PORT}/tcp && sudo ufw deny ${NGINX_PROXY_PORT}/tcp
   fi
 fi
 
